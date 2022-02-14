7795cf9d79bec776f80b963fe1aeb4f7b3ab80c4
ddhormuth0 <69919206+ddhormuth0@users.noreply.github.com>
2022-02-14 15:36:15 -0500

    Update Player1Movement.cs

--- COSC325Game/GameProject/Assets/Scripts/Player1Movement.cs
+++ COSC325Game/GameProject/Assets/Scripts/Player1Movement.cs
@@ -37,15 +37,22 @@ public class Player1Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //attacking
         if (Input.GetKeyDown(KeyCode.Q) && attackTime <= 0)
         {
+            //convert our 2d movment direction vectro into a vector3
             Vector3 directionThree = direction + Vector2.up;
+            //shoots a ray out from the character and detects the item that it hits, only hits players
             RaycastHit2D hit = Physics2D.Raycast(character.gameObject.transform.position + directionThree, direction, 1f, layerMask);
+            //a debug that shows us the swing radius of the sword attack
             Debug.DrawRay(character.gameObject.transform.position + directionThree, direction * 5, Color.red, 3f);
+            //sets the animation state to attack
             animate.SetTrigger("Attack" + 1);
+            //sets a timer of .5 until the next attack can be made
             attackTime = .5f;
             if(hit.collider != null)
             {
+                //debug tool that tells us what we hit with the basic attack
                 Debug.Log("hitting: " + hit.collider.tag);
             }
             
@@ -53,16 +60,22 @@ public class Player1Movement : MonoBehaviour
         //move left
         if (Input.GetKey(KeyCode.A))
         {
+            //vector direction that we are moving in
             direction = Vector2.left/2f;
+            //sets horizontal movement to go left
             horizontalMove = -1f * runSpeed;
+            //sets our animation state to the run animation
             animate.SetInteger("AnimState", 1);
             idolTimer = 0.02f;
         }
         //move right
         else if (Input.GetKey(KeyCode.D))
         {
+            //vector direction we are moving in
             direction = Vector2.right/2;
+            //sets horizontal movement to go right * our movement speed
             horizontalMove = 1f * runSpeed;
+            //sets our animation state to the run animation
             animate.SetInteger("AnimState", 1);
             idolTimer = 0.02f;
         }
@@ -81,7 +94,7 @@ public class Player1Movement : MonoBehaviour
 
         animate.SetFloat("AirSpeedY", m_body2d.velocity.y);
 
-        //for going idol
+        //for going idol and resetting basick attack
         attackTime -= Time.deltaTime;
         idolTimer -= Time.deltaTime;
         if(idolTimer < 0)

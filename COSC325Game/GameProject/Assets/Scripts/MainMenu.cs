8722b1423c6ecd469dd6773f3375b660d40d1813
ddhormuth0 <69919206+ddhormuth0@users.noreply.github.com>
2022-04-07 15:42:30 -0400

    Added Game Loop
    Game can now go from start to finish without having to exit and reenter the game.

--- COSC325Game/GameProject/Assets/Scripts/MainMenu.cs
+++ COSC325Game/GameProject/Assets/Scripts/MainMenu.cs
@@ -11,4 +11,12 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+
+    public void RestartGame()
+    {
+        //takes back to main menu and destroys game manager
+        SceneManager.LoadScene(0);
+        Destroy(GameObject.Find("GameManager"));
+        Destroy(GameObject.Find("CharacterSelection"));
+    }
 }
